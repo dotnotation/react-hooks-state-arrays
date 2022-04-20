d100cf9c96f2f4abe8fd5b48d9df4201b5086bce
dotnotation <dotinmotionproductions@gmail.com>
2022-04-19 21:02:36 -0400

    completed code along

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,20 +3,63 @@ import { spicyFoods, getNewSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] = useState("All");
+  
 
   function handleAddFood() {
     const newFood = getNewSpicyFood();
+    const newFoodArray = [...foods, newFood];
+    setFoods(newFoodArray)
     console.log(newFood);
   }
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+  function handleFilterChange(event) {
+    setFilterBy(event.target.value);
+  }
+  
+  function handleLiClick(id){
+    // how to remove an item from li array
+    // const removedFoodArray = foods.filter((food) => 
+    // food.id !== id);
+    // setFoods(removedFoodArray)
+
+    // increase heat level on specific food
+    const heatChanged = foods.map((food) => {
+    if (food.id === id) {
+      return { 
+        ...food,
+        heatLevel: food.heatLevel +1
+      }
+    } else {
+      return food;
+    }
+    })
+    setFoods(heatChanged)
+  }
+
+  const foodsToDisplay = foods.filter((food) => {
+    if (filterBy === "All") {
+      return true;
+    } else {
+      return food.cuisine === filterBy;
+    }
+  });
+
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={() => handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
 
   return (
     <div>
+    <select name="filter" onChange={handleFilterChange}>
+      <option value="All">All</option>
+      <option value="American">American</option>
+      <option value="Sichuan">Sichuan</option>
+      <option value="Thai">Thai</option>
+      <option value="Mexican">Mexican</option>
+    </select>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
     </div>
